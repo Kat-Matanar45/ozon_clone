aeb08ab5ee8ce9c50ad8d9bb1cb742c34915596b
Kat <avchimchik@gmail.com>
2026-02-06 23:59:31 +0500

    Создаем базу данных

---
+++ lib/db/schema.ts
@@ -0,0 +1,44 @@
+import { sql } from "drizzle-orm/sql/sql";
+import { sqliteTable, text, integer, real } from "drizzle-orm/sqlite-core";
+
+export const users = sqliteTable('users', {
+    id: text('id').primaryKey(),
+    email: text('email').notNull().unique(),
+    password: text('password').notNull(),
+    name: text('name'),
+    createdAt: text('created_at').notNull().default(sql`(CURRENT_TIMESTAMP)`),
+    updatedAt: text('updated_at').notNull().default(sql`(CURRENT_TIMESTAMP)`).$onUpdate(() => sql`(CURRENT_TIMESTAMP)`)
+})
+
+export const sessions = sqliteTable('sessions', {
+    id: text('id').primaryKey(),
+    userId: text('user_id').notNull().references(() => users.id, {onDelete: 'cascade'}),
+    expiresAt: text('expires_at').notNull(),
+})
+
+export const reviews = sqliteTable('reviews', {
+    id: text('id').primaryKey(),
+    userId: text('user_id').notNull().references(() => users.id, {onDelete: 'cascade'}),
+    productId: text('product_id').notNull().references(() => products.id, {onDelete: 'cascade'}),
+    rating: real('rating').notNull(),
+    comment: text('comment'),
+    createdAt: text('created_at').notNull().default(sql`(CURRENT_TIMESTAMP)`)
+})
+
+export const products = sqliteTable('products', { 
+    id: text('id').primaryKey(),
+    name: text('name').notNull(),
+    description: text('description'),
+    price: integer('price').notNull(),
+    discountPrice: integer('discount_price'),
+    image: text('image_url').notNull(),
+    createdAt: text('created_at').notNull().default(sql`(CURRENT_TIMESTAMP)`)
+})
+
+export const orders = sqliteTable('orders', { 
+    id: text('id').primaryKey(),
+    userId: text('user_id').notNull().references(() => users.id, {onDelete: 'cascade'}),
+    total: integer('total').notNull(),
+    status: text('status').notNull().default('pending'),
+    createdAt: text('created_at').notNull().default(sql`(CURRENT_TIMESTAMP)`)
+})
